e3dbb465a8bec89c00950b946a9b5e708913b4c4
B2GPT <132028674+Moderatorboy@users.noreply.github.com>
2026-01-22 20:58:12 +0530

    Create Otherbatch.js

---
+++ Otherbatch.js
@@ -0,0 +1,262 @@
+// Class 13th Data (Fixed Structure)
+const dataClass13 = [
+  {
+    "batch_name": "JEE MAINS 2025 PYQ",
+    // 👇 YEH LINE ADD KARO (Comma zaroor lagana)
+        "channel_id": "-1002011365587",
+    "chapters": [
+      {
+        "chapter_name": "April Attempt",
+        "lectures": [
+          {
+            "lec_no": "L-01",
+            "title": "Unit Dimensions Errors & Vectors ｜ April Attempt Solution",
+            "video_id": 4981,
+            "date": "2025-04-04"
+          },
+          {
+            "lec_no": "L-02",
+            "title": "Kinematics & NLM ｜ April Attempt Solutions",
+            "video_id": 4982,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-03",
+            "title": "WEP⧸COM Collision ⧸ Rotation ｜ April Attempt Solutions",
+            "video_id": 4983
+          },
+          {
+            "lec_no": "L-04",
+            "title": "Gravitation & SHM ｜ April Attempt Solutions",
+            "video_id": 4984
+          },
+          {
+            "lec_no": "L-05",
+            "title": "Solids & Fluids ｜ April Attempt Solution",
+            "video_id": 4985
+          },
+          {
+            "lec_no": "L-06",
+            "title": "KTG & Thermodynamics ｜ April Attempt Solution",
+            "video_id": 4986
+          },
+          {
+            "lec_no": "L-07",
+            "title": "Thermal Properties & Wave Motion ｜ April Attempt Solution ",
+            "video_id": 4987
+          },
+          {
+            "lec_no": "L-08",
+            "title": "Electrostatics ｜ April Attempt Solutions",
+            "video_id": 4988,
+            "date": "2025-04-04"
+          },
+          {
+            "lec_no": "L-09",
+            "title": "Capacitors & Current Electricity ｜ April Attempt Solution",
+            "video_id": 4989,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-10",
+            "title": "Moving Charges & MEC ｜ April Attempt Solution",
+            "video_id": 4990
+          },
+          {
+            "lec_no": "L-11",
+            "title": "EMI & AC ｜ April Attempt Solution",
+            "video_id": 4991
+          },
+          {
+            "lec_no": "L-12",
+            "title": "Ray Optics ｜ April Attempt Solution",
+            "video_id": 4992
+          },
+          {
+            "lec_no": "L-13",
+            "title": "EM Waves Wave Optics & Semiconductors",
+            "video_id": 4993
+          },
+          {
+            "lec_no": "L-14",
+            "title": "Modern Physics ｜ April Attempt Solution",
+            "video_id": 4994
+          }
+        ]
+      },
+      {
+        "chapter_name": "Jan Attempt",
+        "lectures": [
+          {
+            "lec_no": "L-01",
+            "title": "Wave Optics Thermal Properties & Heat Transfer ｜ Jan Attempt Solution",
+            "video_id": 4995,
+            "date": "2025-04-04"
+          },
+          {
+            "lec_no": "L-02",
+            "title": "Modern Physics & Semiconductors ｜ Jan Attempt Solutions",
+            "video_id": 4996,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-03",
+            "title": "EMI, AC & EM Waves ｜ Jan Attempt Solutions",
+            "video_id": 4997
+          },
+          {
+            "lec_no": "L-04",
+            "title": "U&D, Errors, KTG-Thermodynamics & Wave Motion ｜ Jan Attempt Solutions",
+            "video_id": 4998
+          },
+          {
+            "lec_no": "L-05",
+            "title": "Gravitation Solids Fluids & SHM ｜ Jan Attempt Solutions",
+            "video_id": 4999
+          },
+          {
+            "lec_no": "L-06",
+            "title": "Current Electricity, Moving Charges & MEC ｜ Jan Attempt Solutions",
+            "video_id": 5000
+          },
+          {
+            "lec_no": "L-07",
+            "title": "Electrostatics & Capacitors ｜ Jan Attempt Solutions",
+            "video_id": 5001
+          },
+          {
+            "lec_no": "L-08",
+            "title": "Kinematics⧸NLM⧸WEP⧸COM⧸Rotation ｜ Jan Attempt Solutions",
+            "video_id": 5002,
+            "date": "2025-04-04"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "batch_name": "JEE MAINS 2024 PYQ",
+    // 👇 YEH LINE ADD KARO (Comma zaroor lagana)
+        "channel_id": "-1002011365587",
+    "chapters": [
+      {
+        "chapter_name": "Jan Attempt",
+        "lectures": [
+          {
+            "lec_no": "L-01",
+            "title": "U&D Errors Wave Motion & Thermal Properties ｜ Jan Attempt Solutions",
+            "video_id": 5003,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-02",
+            "title": "Kinematics & NLM ｜ Jan Attempt Solutions",
+            "video_id": 5004
+          },
+          {
+            "lec_no": "L-03",
+            "title": "WEP COM Collision Rotation Gravitation ｜ Jan Attempt Solutions",
+            "video_id": 5005
+          },
+          {
+            "lec_no": "L-04",
+            "title": "Thermodynamics EMI & AC ｜ Jan Attempt Solutions",
+            "video_id": 5006
+          },
+          {
+            "lec_no": "L-05",
+            "title": "Electrostatics & Capacitors ｜ Jan Attempt Solutions",
+            "video_id": 5007
+          },
+          {
+            "lec_no": "L-06",
+            "title": "Current Electricity Moving Charges & MEC ｜ Jan Attempt Solutions",
+            "video_id": 5008
+          },
+          {
+            "lec_no": "L-07",
+            "title": " Ray Optics Wave Optics EM Waves ｜ Jan Attempt Solutions",
+            "video_id": 5009,
+            "date": "2025-04-04"
+          },
+          {
+            "lec_no": "L-08",
+            "title": "Modern Physics & Semiconductors ｜ Jan Attempt Solutions",
+            "video_id": 5010,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-09",
+            "title": "Solids, Fluids & SHM ｜ Jan Attempt Solutions",
+            "video_id": 5011
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "batch_name": "BEAST GAME",
+    // 👇 YEH LINE ADD KARO (Comma zaroor lagana)
+        "channel_id": "-1002011365587",
+    "chapters": [
+      {
+        "chapter_name": "SEASON 01",
+        "lectures": [
+          {
+            "lec_no": "L-01",
+            "title": "EPISODE 01",
+            "video_id": 5013,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-02",
+            "title": "EPISODE 02",
+            "video_id": 5014
+          },
+          {
+            "lec_no": "L-03",
+            "title": "EPISODE 03",
+            "video_id": 5015
+          },
+          {
+            "lec_no": "L-04",
+            "title": "EPISODE 04",
+            "video_id": 5016
+          },
+          {
+            "lec_no": "L-05",
+            "title": "EPISODE 05",
+            "video_id": 5017
+          },
+          {
+            "lec_no": "L-06",
+            "title": "EPISODE 06",
+            "video_id": 5018
+          },
+          {
+            "lec_no": "L-07",
+            "title": "EPISODE 07",
+            "video_id": 5020,
+            "date": "2025-04-04"
+          },
+          {
+            "lec_no": "L-08",
+            "title": "EPISODE 08",
+            "video_id": 5021,
+            "date": "2024-01-05"
+          },
+          {
+            "lec_no": "L-09",
+            "title": "EPISODE 09",
+            "video_id": 5022
+          },
+          {
+            "lec_no": "L-10",
+            "title": "EPISODE 10",
+            "video_id": 5024
+          }
+        ]
+      }
+    ]
+  }
+];
